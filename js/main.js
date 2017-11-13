6382daff80c870f512df991fdac3dbf572f9dd48
Matt Bond <mattbond@asana.com>
2017-11-13 15:01:12 -0800

    new stuff

--- js/main.js
+++ js/main.js
@@ -19,3 +19,14 @@ $(".radio").click(function(){
 $(document).ready(function() {
   $('.drawer').drawer();
 });
+
+$(function() {
+    $('ul.o-list-ui li a').on('click', function(e){
+        var $link = $(this);
+        var $parentNode = $link.parent();
+        var linkLeavesPage = $link.attr('href');
+        if (!linkLeavesPage) {
+            $parentNode.toggleClass('nav-tree-node-is-open');
+        }
+    });
+});
